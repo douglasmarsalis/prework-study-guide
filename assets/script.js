6554e0aed8ea03c6e7ef5b4cff0ad3540fe97d38
Douglas <japanmarsalis@gmail.com>
2023-04-26 16:16:10 -0500

    Update with tutor

--- assets/script.js
+++ assets/script.js
@@ -7,15 +7,18 @@ function listTopics() {
 }
 
 function selectTopic() {
- if (topic === 'HTML') {
+ if (topics === 'HTML') {
    console.log("Let's study HTML!");
- } else if (topic === 'CSS') {
+ } else if (topics === 'CSS') {
    console.log("Let's study CSS!");
- } else if (topic === 'Git') {
+ } else if (topics === 'Git') {
    console.log("Let's study Git!");
- } else if (topic === 'JavaScript') {
+ } else if (topics === 'JavaScript') {
    console.log("Let's study JavaScript!");
  } else {
    console.log('Please try again!');
  }
 }
+
+listTopics()
+selectTopic()
